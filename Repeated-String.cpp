656cd691856f2c11c6a2b5319f535966aae2d725
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-02-27 02:44:31 +0900

    Update Repeated-String.cpp

--- Repeated-String.cpp
+++ Repeated-String.cpp
@@ -1,6 +1,12 @@
+int main(void)
+{
+    return 0;
+}
+
 long repeatedString(string s, long n)
 {
     long result = 0;
+    
     long size = std::count(s.begin(), s.end(), 'a') * (n/s.size());
     long rest = std::count(s.begin(), s.begin() + (n % s.size()), 'a');
     
